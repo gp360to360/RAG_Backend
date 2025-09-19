a5db8fc059a904ec0cfa3623e115de0fd487a33f
gyanendra prajapati <99497106+gp360to360@users.noreply.github.com>
2025-09-19 13:37:56 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
